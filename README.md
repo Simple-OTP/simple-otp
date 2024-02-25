255f0dbf8e3d476235d57021daaa90a1b98300e6
Ned Wolpert <ned.wolpert@gmail.com>
2024-02-25 10:38:34 -0700

    Started project to replace authy

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# simple_otp
+# Simple OTP
+
+Now that Authy no longer has a desktop application, planning on creating a way 
+to do this easily from the stored set of secrets.
+
+Using rust for the application. End goal will be a simple UI where you can 
+enter the secrets. They will be stored locally in a JSON file, encrypted with 
+your passphrase. (Or use gpg to encrypt the file, still trying to decide.)
+
+Adding a UI with a count-down timer because that's fun.
+
+# Install GTK3
+
+## Ubunto
+```bash
+sudo apt-get install libgtk-3-dev
+```
+
+# Libraries in use
+- [totp-rs](https://github.com/constantoine/totp-rs) for OTP generation
+- [druid](https://github.com/linebender/druid) for the UI
---
+++ otp/src/main.rs
@@ -0,0 +1,29 @@
+use std::time::SystemTime;
+use totp_rs::{Algorithm, TOTP, Secret};
+use std::env;
+
+fn main() {
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <secret>", args[0]);
+        return;
+    }
+
+    let time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+
+    let mut secret = Secret::Encoded(args[1].to_string()).to_bytes().unwrap();
+    if (secret.len()<16) {
+        secret.resize(16, 0);
+    }
+    println!("length: {}", secret.len());
+    let totp = TOTP::new(
+        Algorithm::SHA1,
+        6,
+        1,
+        30,
+        secret,
+    ).unwrap();
+    let token = totp.generate_current().unwrap();
+    println!("{} : {}", token, time % 30);
+}
