1c098a73891cad04eb8d2f914b60820a19423ac9
Ned Wolpert <ned.wolpert@gmail.com>
2024-02-27 07:47:45 -0700

    First pass at learning druid

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -1,22 +1,57 @@
-use druid::widget::{Button, Flex, Label};
-use druid::{AppLauncher, LocalizedString, PlatformError, Widget, WidgetExt, WindowDesc};
+use druid::widget::{Button, Align, Flex, Label, TextBox};
+use druid::{AppLauncher, LocalizedString, Widget, WidgetExt, WindowDesc, Data, Env, Lens};
+use std::env;
 
-fn main() -> Result<(), PlatformError> {
-    let main_window = WindowDesc::new(ui_builder());
-    let data = 0_u32;
-    AppLauncher::with_window(main_window)
-        .log_to_console()
-        .launch(data)
+const VERTICAL_WIDGET_SPACING: f64 = 20.0;
+const TEXT_BOX_WIDTH: f64 = 200.0;
+const WINDOW_TITLE: LocalizedString<TOTPState> = LocalizedString::new("One Time Password!");
+
+#[derive(Clone, Data, Lens)]
+struct TOTPState {
+    secret: String,
+    token: String,
 }
 
-fn ui_builder() -> impl Widget<u32> {
-    // The label text will be computed dynamically based on the current locale and count
-    let text =
-        LocalizedString::new("hello-counter").with_arg("count", |data: &u32, _env| (*data).into());
-    let label = Label::new(text).padding(5.0).center();
-    let button = Button::new("increment")
-        .on_click(|_ctx, data, _env| *data += 1)
-        .padding(5.0);
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <secret>", args[0]);
+        return;
+    }
 
-    Flex::column().with_child(label).with_child(button)
+    let main_window = WindowDesc::new(build_root_widget())
+        .title(WINDOW_TITLE)
+        .window_size((400.0, 400.0));
+
+    let initial_state = TOTPState {
+        secret: args[1].clone(),
+        token: otp::otp_for(&args[1]).unwrap().into(),
+    };
+
+    // start the application
+    AppLauncher::with_window(main_window)
+        .launch(initial_state)
+        .expect("Failed to launch application");
+}
+
+
+
+fn build_root_widget() -> impl Widget<TOTPState> {
+
+    // a label that will determine its text based on the current app data.
+    let label = Label::new(|data: &TOTPState, _env: &Env| format!("OTP {}", data.token));
+    // a textbox that modifies `name`.
+    let textbox = TextBox::new()
+        .with_placeholder("Who are we greeting?")
+        .fix_width(TEXT_BOX_WIDTH)
+        .lens(TOTPState::token);
+
+    // arrange the two widgets vertically, with some padding
+    let layout = Flex::column()
+        .with_child(label)
+        .with_spacer(VERTICAL_WIDGET_SPACING)
+        .with_child(textbox);
+
+    // center the two widgets in the available space
+    Align::centered(layout)
 }
