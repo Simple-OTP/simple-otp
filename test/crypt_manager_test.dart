82f4de534e995a99831698bd746e4e9ca7c7595c
Ned Wolpert <ned.wolpert@gmail.com>
2024-04-18 07:10:06 -0700

    Moved tests around

--- test/crypt_manager_test.dart
+++ test/crypt_manager_test.dart
@@ -1,6 +1,7 @@
 import 'package:cryptography/cryptography.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:simple_otp/manager/crypt_manager.dart';
+import 'package:simple_otp/model/otp_secret.dart';
 
 void main() {
   runAssertion(ByteManager mgr) async {
@@ -24,4 +25,20 @@ void main() {
     final ByteManager mgr = PlainManager();
     runAssertion(mgr);
   });
+
+  testWidgets("Test database encryption/decryption",
+          (WidgetTester tester) async {
+        // Test the toString method of the Nonce class
+        SecretKey secretKey = await AesGcm.with256bits().newSecretKey();
+        final ByteManager mgr = CryptManager(secretKey);
+        final List<OTPSecret> list = [
+          OTPSecret(issuer: "issuer", username: "username", secret: "secret"),
+          OTPSecret(issuer: "issuer2", username: "username2", secret: "secret2"),
+        ];
+        final jsonString = OTPSecret.writeToJSON(list);
+        final encrypted = await mgr.toBytes(jsonString);
+        final decrypted = await mgr.fromBytes(encrypted);
+        expect(jsonString, decrypted);
+      });
+
 }
