d139a8ca2ed4f6f5f0ddafa7376f8f051391bcc8
Ned Wolpert <ned.wolpert@gmail.com>
2026-01-29 07:27:05 -0700

    Why is file picker so bad? Fix a fix to their previous fix...

--- lib/routes/database_route.dart
+++ lib/routes/database_route.dart
@@ -97,7 +97,7 @@ class DatabaseRoute extends StatelessWidget {
 
   void doExport(final SecretList secretList) {
     final String json = OTPSecret.writeToJSON(secretList.otpSecrets);
-    FilePicker
+    FilePicker.platform
         .saveFile(
             fileName: 'simple_otp.json',
             allowedExtensions: ['json'],
@@ -117,7 +117,7 @@ class DatabaseRoute extends StatelessWidget {
   void doImport(
       final SecretList secretList, final void Function(Object) onError) async {
     try {
-      FilePickerResult? result = await FilePicker.pickFiles(
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
         allowedExtensions: ['json', 'jsn'],
       );
       if (result != null &&
